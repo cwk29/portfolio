61cba964168fff2f2dde103c101095ead3193a69
Cody Kuykendall <cwk29@icloud.com>
2025-03-22 20:07:44 -0400

    Updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,11 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Cody Kuykendall CV",
+  title: "Cody Kuykendall",
   description: "Hello, I'm Cody.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
